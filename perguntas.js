08c4ac4c65089a5d33ce54bed89709cfb2c1e718
manumariayas <143712731+manumariayas@users.noreply.github.com>
2024-09-17 08:14:37 -0300

    l

---
+++ perguntas.js
@@ -0,0 +1,81 @@
+    criaCartao( 
+    'Matemática', 
+    'Qual a formula para se calcular o volume de um cubo', 
+    'lado ao cubo') 
+    criaCartao( 
+    'Geografia', 
+    'Tipo de relevo do estado paran?', 
+    'é de elevada altitude, formado por planaltos, além de formações de serra, como a Serra Geral e a Serra do Mar') 
+    criaCartao( 
+    'História', 
+    'Em que ano ocorreu a revolução francesa?', 
+    'Século XVII') 
+    criaCartao( 
+    'Matemática ',
+    'Qual formula se usa para calcular a soma de dois vetores com abertura de um ângulo qualquer', 
+    'Lei dos cossenos') 
+    criaCartao( 
+    'Quimica', 
+    'Qual radical caracteriza um álcool', 
+    'Hidroxila') 
+    criaCartao( 
+    'Matemática', 
+    'Qual a fórmula da soma de uma progressão aritmética', 
+    '(a1 + an) X an/2') 
+    criaCartao( 
+    'História', 
+    'Quando foi declarada a abolição da escravidão', 
+    '1888') 
+    criaCartao( 
+    'Filosofia', 
+    'Qual a ideia principal', 
+    'Real Madrid') 
+    criaCartao( 
+    'Futebol', 
+    'Na edição da Champions league de 2015, quem foi o autor dos gol na final?', 
+    'Neymar e messi') 
+    criaCartao( 
+    'Futebol', 
+    'Quem foi campeão da última edição da Premier league?', 
+    'Manchester city') 
+    criaCartao( 
+    'Língua estrangeira', 
+    'Qual a origem da língua inglesa?', 
+    'Anglo-saxônica') 
+    criaCartão( 
+    'Língua Portuguesa', 
+    '"Muito" é um advérbio de:', 
+    'Intensidade') 
+    criaCartao( 'Matemática', 
+    'Qual o valor da integrada de um número inteiro?', 
+    '0') 
+    criaCartao( 
+    'Biologia', 
+    'O nome da síndrome causada pela presença de um cromossomo 23 a mais?', 
+    'Síndrome de klinefelter') 
+    criaCartao( 'Biologia', 
+    'Qual o tipo de respiração realizada por anfíbios?', 
+    'branquial, pulmonar ou cutânea, dependendo da espécie e fase de vida') 
+    criaCartao( 
+    'Geografia', 
+    'interespaço transicional brasileiro, uma floresta ombrófila aberta que fica entre a Floresta amazônica, o Cerrado e a Caatinga', 
+    'Mata dos cocais') 
+    criaCartao( 
+    'Matemática', 
+    'Qual a formula para calcular o volume de um cone?', 'Área da base × altura / 3') 
+    criaCartao( 
+    'Japonês', 
+    'Quantos alfabetos são utilizados na língua japonesa hoje?', 'Três') 
+    criaCartao( 
+    'Inglês', 
+    'I\'m 7 years old. Maria is 10 years old. John is not 9 years old. How old is john?', 
+    '8 years old') 
+    criaCartao( 
+    'Literatura', 
+    'Quantos contos noite na taverna tem?', 
+    '5 contos e dois capítulos em terceira pessoa') 
+    criaCartao( 
+    'Extra', 
+    'Qual a linguagem de programação feita para crianças?', 
+    'Python') 
+    
