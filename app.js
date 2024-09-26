ca1e637de3c7ba02e608cbd721b7fc73fd37396f
manumariayas <143712731+manumariayas@users.noreply.github.com>
2024-09-26 08:44:34 -0300

    k

---
+++ app.js
@@ -0,0 +1,18 @@
+criaCartao
+function criaCartao (categoria, pergunta, resposta){
+let conteiner = documen.getElementBgId('contener')
+let cartao = document.criateElement ('article')
+cartao.classNome= 'cartao'
+cartao.innerHtml= 
+<div class ="cartao-conteudo">
+<h3>${categoria}</h3>
+<div class="cartao-pergunta">
+<p>${pergunta}</p>
+</div>
+<div class="cartao-resposta">
+    <p>${resposta}</p>
+    </div>
+</div>
+
+{contener.appendChild(cartao);
+}
