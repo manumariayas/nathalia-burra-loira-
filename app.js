e99610e60690a15dab0c54ef9aa3e141bde0cc68
manumariayas <143712731+manumariayas@users.noreply.github.com>
2024-10-03 08:44:11 -0300

    l

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function criaCartao(categoria, pergunta, resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    cartao.innerHTML = 
+    cartao.innerHTML = `
     <div class="conteudo-cartao">
     <h3>${categoria}</h3>
     <div class="cartao-pergunta">
@@ -11,7 +11,7 @@ function criaCartao(categoria, pergunta, resposta){
     <div class="cartao-pergunta">
         <p>${resposta}</p>
     </div>
-    </div>
+    </div>`
     let respostaVisivel = false
     function viraCartao(){
         respostaVisivel = !respostaVisivel;
