016224cfb4a377ae98dfc5875db7968274e42675
manumariayas <143712731+manumariayas@users.noreply.github.com>
2024-10-22 10:39:14 -0300

    r

--- projetos2.js
+++ projetos2.js
@@ -3,7 +3,7 @@ function criaCartao(categoria, pergunta, resposta){
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = `
-    <div class="conteudo-cartao" onclick= "mostrarResposta">
+    <div class="conteudo-cartao" onclick= "mostrarResposta(this)">
     <h3>${categoria}</h3>
     <div class="cartao-pergunta">
         <p>${pergunta}</p>
